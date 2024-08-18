0d1060fce7d7555a44b3ab732f39a2fc50340267
kowshik jallipalli <119750794+kowshikjallipalli0105@users.noreply.github.com>
2024-08-18 21:51:00 +0530

    Time: 0 ms (100%), Space: 11.9 MB (44.6%) - LeetHub

--- 1241-decompress-run-length-encoded-list/1241-decompress-run-length-encoded-list.cpp
+++ 1241-decompress-run-length-encoded-list/1241-decompress-run-length-encoded-list.cpp
@@ -5,9 +5,9 @@ public:
         vector<int>res;
         for(int i=0;i<n;i+=2){
             int freq=nums[i];
-            // int val=nums[i+1];
+            int val=nums[i+1];
             for(int j=0;j<freq;j++){
-                res.push_back(nums[i+1]);
+                res.push_back(val);
             }
 
 
