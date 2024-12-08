85a5d3425ac5931246fb7a48fca0a8850aa42793
kowshik jallipalli <119750794+kowshikjallipalli0105@users.noreply.github.com>
2024-12-08 15:47:53 +0530

    Time: 6 ms (20.18%), Space: 8.7 MB (12.38%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
+        int count=0;
         int temp=n;
         for(int i=1;i<=n;i++){
             temp=temp-i;
             if(temp<0){
                 return i-1;
             }
-            
         }
         return 1;
     }
